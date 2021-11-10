c42993e070c260b0f76ac2a6fa439622003ce28b
dakyChv <daki.litd@gmail.com>
2021-11-10 20:41:33 +0700

    create excute(): implement sdk,db,echo server

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+	"os/signal"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	"gitlab.com/jdb.com.la/sdk-go"
 )
 
 // GetEnv returns the environment variable or default value.
@@ -44,8 +50,72 @@ var (
 		PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE, timeoutOption)
 )
 
+func main() {
+	if err := excute(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
 
-func main() {}
+func excute() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	sdk, err := sdk.New(ctx, &sdk.Config{
+		BaseURL:   BANK_BASE_URL,
+		APIKey:    BANK_API_KEY,
+		SecretKey: BANK_SECRET_KEY,
+		HMACKey:   []byte(BANK_HMACKEY),
+	})
+	if err != nil {
+		return fmt.Errorf("create sdk client failure: %v", err)
+	}
+	defer sdk.Close()
+
+	aqClient, err := sdk.TapWater(ctx)
+	if err != nil {
+		return fmt.Errorf("create jdb tapwater client failure: %v", err)
+	}
+
+	db, err := sqlx.Open("postgres", PGDSN)
+	if err != nil && db.Ping() != nil {
+		return fmt.Errorf("open database failure: %v", err)
+	}
+
+	aqService := &AquaService{
+		db:       db,
+		aqClient: aqClient,
+	}
+
+	h := &HTTPTransport{
+		aqService: aqService,
+	}
+
+	e := newEchoServer()
+	h.install(e)
+
+	errCh := make(chan error, 1)
+	ctx, cancel = signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	go func() {
+		errCh <- e.Start(ADDR)
+	}()
+
+	select {
+	case <-ctx.Done():
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := e.Shutdown(ctx); err != nil {
+			return fmt.Errorf("shutdown server failure: %v", err)
+		}
+		fmt.Println("server shutdown")
+	case err := <-errCh:
+		return fmt.Errorf("start server error: %v", err)
+	}
+
+	return nil
+}
 
 func newEchoServer() *echo.Echo {
 	e := echo.New()
