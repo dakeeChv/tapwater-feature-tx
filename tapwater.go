e36b7ee19ea2878cb8a8ac38cd76560899e8b654
dakyChv <daki.litd@gmail.com>
2021-11-11 18:10:59 +0700

    add handling the error: Info()

--- tapwater.go
+++ tapwater.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	sdk "gitlab.com/jdb.com.la/sdk-go/billing/tapwater"
@@ -10,6 +11,10 @@ import (
 
 // ErrNoInfo is returned when no info is found.
 var ErrNoInfo = errors.New("no info")
+
+// ErrNotAllowCcy is return when currency is foreign for fix transaction amount
+// Reference: https://litd-team.monday.com/boards/1305312982/pulses/1821535994?asset_id=316617661
+var ErrNotAllowCcy = errors.New("not allow foreign currency for this bill type")
 
 // ErrPermissionDenied is returned when the user
 // does not have permission to perform the action.
@@ -31,8 +36,12 @@ func (aq *AquaService) Province(ctx context.Context) ([]Province, error) {
 func (aq *AquaService) Info(ctx context.Context, in InfoQuery) (Info, error) {
 	info, err := aq.aqClient.Info(ctx, in)
 	if err != nil {
-		if err.Error() != "" {
+		var lowerErr = strings.ToLower(err.Error())
+		if strings.Contains(lowerErr, "invalid customer no") || strings.Contains(lowerErr, "invalid province no") {
 			return Info{}, ErrNoInfo
+		}
+		if strings.Contains(lowerErr, "account currency not allow") {
+			return Info{}, ErrNotAllowCcy
 		}
 		return Info{}, err
 	}
